f3dea3fabef9a337b60fe308871bcd3f5a587687
sachinimadara <sdsmadara@gmail.com>
2016-11-03 13:36:59 +0530

    update calculator.java
    variable declared

--- Calculator.java
+++ Calculator.java
@@ -15,7 +15,7 @@ public class Calculator implements CalDB {
 
 
     @Override
-public int add(Scanner a, Scanner b) {
+public int add(Scanner c, Scanner b) {
     int n1=Integer.parseInt(a.next());
     int n2=Integer.parseInt(b.next());
     return  (n1 + n2);
